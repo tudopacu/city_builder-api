974be8ccbd346f7682997d6092443ab92515753b
tudor.pascu <tudor.pascu@booking.com>
2026-06-23 11:17:20 +0300

    site and game url updates

--- configuration/config.go
+++ configuration/config.go
@@ -1,7 +1,7 @@
 package configuration
 
-const GameURL = "http://localhost:8080"
-const SiteURL = "http://localhost:5173"
+const GameURL = "http://citybuilder.home.ro:8083"
+const SiteURL = "http://citybuilder.home.ro:8082"
 
 const RedisAddr = "redis-service:6379"
 const RedisPassword = ""
